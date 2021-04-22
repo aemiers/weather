3d68d107d61378f978ff8fbd75e7f150e96e03f4
Anneke Miers <annekemiers.am@gmail.com>
2021-04-21 19:51:58 -0600

    Citydata loading in app

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,18 +1,42 @@
-import './App.scss';
-
+import './App.scss'
+import { fetchCityId, fetchWeatherById } from '../../data/apiCalls'
 import React, { Component } from 'react';
 
 class App extends Component {
   constructor() {
     super()
     this.state = {
-      error: '',
-      weatherData: []
+      cityData: [],
+      weatherData: [],
+      error: ''
     }
   }
 
   componentDidMount() {
-    fetchWeatherData()
-      .then(())
+    fetchCityId('Denver')
+      .then((fetchedCityData) => {
+        this.setState({ cityData: fetchedCityData })
+      })
+      .catch(error => this.setState({ error: 'There was a loading error. Please reload the page and try again.' }))
+    // .then(fetchWeatherById(this.cityData))
+    // fetchWeatherData()
+    //   .then((weathers) => {
+    //     this.setState({weatherData: this.prepWeatherData(weathers)})
+    //   })
+  }
+
+  render() {
+    return (
+      <main className='app'>
+        {!!this.state.error &&
+          <h2 className="error-feedback">{this.state.error}</h2>
+        }
+
+        <section className='main-page'>
+        </section>
+      </main>
+    )
   }
 }
+
+export default App;
--- src/data/apiCalls.js
+++ src/data/apiCalls.js
@@ -1,11 +1,19 @@
-const baseURL = 'https://www.metaweather.com/api/location/'
+// const baseURL = 'https://www.metaweather.com/api/location/'
+const baseURL = '/api/location/'
 
-export const fetchCityId = () => {
-  return fetch(`${baseURL}search/?query=london`)
+export const fetchCityId = (cityName) => {
+  return fetch(`${baseURL}search/?query=${cityName}`)
     .then(response => {
       return response.json()
     })
 }
+
+// export const fetchCityId = (cityName) => {
+//   let allWeatherData;
+//   let cityData = fetch(`${baseURL}search/?query=${cityName}`)
+//     .then(response => response.json())
+//   let weatherData = fetch(`${baseURL}${id}`)
+// }
 
 export const fetchWeatherById = (id) => {
   return fetch(`${baseURL}${id}`)
