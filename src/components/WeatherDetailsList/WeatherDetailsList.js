ebe583ff96c7ddc906b9eeb3421fa04047968d4c
Anneke Miers <annekemiers.am@gmail.com>
2021-04-25 13:04:01 -0600

    Compolete daily forecast page

--- src/components/WeatherDetailsList/WeatherDetailsList.js
+++ src/components/WeatherDetailsList/WeatherDetailsList.js
@@ -1,21 +1,42 @@
 import './WeatherDetailsList.scss';
 import React from 'react';
+import compass from '../../assets/compass.svg'
+import moistureDrops from '../../assets/032-drop.svg'
+import windSock from '../../assets/windsock.svg'
+import windPressure from '../../assets/011-wind.svg'
+import cloud from '../../assets/cloud.svg'
+
 
 const WeatherDetailsList = ({ weatherData }) => {
   let humidity, wind, windDirection, pressure, visibility;
   if (weatherData.weatherForecast) {
     humidity = weatherData.weatherForecast[0].humidity;
-    wind = Math.round(weatherData.weatherForecast[0].wind_speed);
     pressure = weatherData.weatherForecast[0].air_pressure;
     visibility = Math.round(weatherData.weatherForecast[0].visibility);
   }
   return (
     <section className='details-card-container'>
-      <p className='weather-details'>{humidity}</p>
-      <p className='weather-details'>{wind}</p>
-      <p className='weather-details'>{windDirection}</p>
-      <p className='weather-details'>{pressure}</p>
-      <p className='weather-details'>{visibility}</p>
+      <div className='detail-container'>
+        <img src={moistureDrops} alt='favorite' className='weather-details-icon' />
+        <p className='weather-details'>Humidity {humidity}%</p>
+      </div>
+      <div className='detail-container'>
+        <img src={windSock} alt='favorite' className='weather-details-icon' />
+        <p className='weather-details'>Wind Speed {wind}MPH</p>
+      </div>
+      <div className='detail-container'>
+        <img src={compass} alt='favorite' className='weather-details-icon' />
+        <p className='weather-details'>Wind Direction {windDirection}</p>
+      </div>
+      <div className='detail-container'>
+        <img src={windPressure} alt='favorite' className='weather-details-icon' />
+        <p className='weather-details'>Air Pressure {pressure}</p>
+      </div>
+      <div className='detail-container'>
+        <img src={cloud} alt='favorite' className='weather-details-icon' />
+        <p className='weather-details'>Visibility {visibility}%</p>
+      </div>
+
 
     </section>
   )
